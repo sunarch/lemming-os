88ce7f4bf6243ea722a7c27c7b46a9ea644c9ce8
Andras Nemeth <nemeth.andras.productions@gmail.com>
2015-06-21 10:50:39 +0200

    Add macros for ASCII characters

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 #nasm "./bootloader/bootloader.asm" -i "./bootloader/" -f bin -o "./build/bootloader.bin"  # assemble bootloader
 #nasm "./kernel/kernel.asm" -i "./kernel/" -f bin -o "./build/kernel.bin"  # assemble kernel
 
-nasm "./LemmingOS.asm" -i "./bootloader/" -i "./kernel/" -f bin -o "./build/LemmingOS.bin"   # assemble OS
+nasm "./LemmingOS.asm" -i "./common/" -i "./bootloader/" -i "./kernel/" -f bin -o "./build/LemmingOS.bin"   # assemble OS
 
 #dd if="build/bootloader.bin" of="build/LemmingOS.hdd" bs=512  # create a bootable hdd image
 #dd if="build/kernel.bin" of="build/LemmingOS.hdd" obs=512 skip=1 # create a bootable hdd image
