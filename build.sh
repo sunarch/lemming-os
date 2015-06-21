02f87bae85bf3618cc70c77687b0841e284487a4
Andras Nemeth <nemeth.andras.productions@gmail.com>
2015-06-21 09:10:35 +0200

    Adjust build commands

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
-nasm "./LemmingOS.asm" -i "./bootloader/" -i "./kernel/" -f bin -o "./build/LemmingOS.bin"   # assemble kernel
-#dd if="build/LemmingOS.bin" of="build/LemmingOS.hdd"  # create a bootable floppy image
+#nasm "./bootloader/bootloader.asm" -i "./bootloader/" -f bin -o "./build/bootloader.bin"  # assemble bootloader
+#nasm "./kernel/kernel.asm" -i "./kernel/" -f bin -o "./build/kernel.bin"  # assemble kernel
+
+nasm "./LemmingOS.asm" -i "./bootloader/" -i "./kernel/" -f bin -o "./build/LemmingOS.bin"   # assemble OS
+
+#dd if="build/bootloader.bin" of="build/LemmingOS.hdd" bs=512  # create a bootable hdd image
+#dd if="build/kernel.bin" of="build/LemmingOS.hdd" obs=512 skip=1 # create a bootable hdd image
+#dd if="build/LemmingOS.bin" of="build/LemmingOS.flp"  # create a bootable floppy image
+
 dd if="build/LemmingOS.bin" of="build/LemmingOS.hdd"  # create a bootable hdd image
