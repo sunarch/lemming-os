9028140608a59209fdacaad9557d17b6393e7e40
Andras Nemeth <nemeth.andras.productions@gmail.com>
2015-06-21 10:50:39 +0200

    Add macros for ASCII characters

--- bootloader/data.asm
+++ bootloader/data.asm
@@ -1,6 +1,7 @@
 
-welcome          db  'Welcome to LemmingOS!', 0x0D, 0x0A, 0
-badcommand       db  'Bad command entered.', 0x0D, 0x0A, 0
-prompt           db  '>', 0
-buffer times 64  db  0
-version          db  "v.0.1.1 Alpha"
+welcome     db  'Welcome to LemmingOS!', ASCII_CR, ASCII_LF, ASCII_NUL
+badcommand  db  'Bad command entered.', ASCII_CR, ASCII_LF, ASCII_NUL
+prompt      db  '>', ASCII_NUL
+version     db  "LemmingOS v.0.1.1 Alpha", ASCII_CR, ASCII_LF, ASCII_NUL
+
+buffer  times 64  db  0
