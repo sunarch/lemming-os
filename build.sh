41225563ec91096c93367b3e8156efa03e28f139
Andras Nemeth <nemeth.andras.productions@gmail.com>
2015-09-14 14:33:44 +0200

    Rename main file after standards

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 #nasm "./bootloader/bootloader.asm" -i "./bootloader/" -f bin -o "./build/bootloader.bin"  # assemble bootloader
 #nasm "./kernel/kernel.asm" -i "./kernel/" -f bin -o "./build/kernel.bin"  # assemble kernel
 
-nasm "./LemmingOS.asm" -i "./common/" -i "./bootloader/" -i "./kernel/" -f bin -o "./build/LemmingOS.bin"   # assemble OS
+nasm "./lemming-os.asm" -i "./common/" -i "./bootloader/" -i "./kernel/" -f bin -o "./build/lemming-os.bin"   # assemble OS
 
 #dd if="build/bootloader.bin" of="build/LemmingOS.hdd" bs=512  # create a bootable hdd image
 #dd if="build/kernel.bin" of="build/LemmingOS.hdd" obs=512 skip=1 # create a bootable hdd image
 #dd if="build/LemmingOS.bin" of="build/LemmingOS.flp"  # create a bootable floppy image
 
-dd if="build/LemmingOS.bin" of="build/LemmingOS.hdd"  # create a bootable hdd image
+dd if="build/lemming-os.bin" of="build/lemming-os.hdd"  # create a bootable hdd image
