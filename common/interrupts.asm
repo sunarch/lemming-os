35b5888bd9e9662a4d2a8aeecf209a97dfc1a1e7
András Németh <sunarchx@gmail.com>
2017-03-22 22:08:54 +0100

    Change hex format from h to 0x

--- common/interrupts.asm
+++ common/interrupts.asm
@@ -14,32 +14,32 @@
 ; interrupt vector 10h - Video services
 ; ========================================
 
-%define  INT_VIDEO  10h  ; interrupt vector for interrupt call
+%define  INT_VIDEO  0x10  ; interrupt vector for interrupt call
 
-%define  INT_VIDEO_FUNC_00h  00h  ; Set Video Mode
-%define  INT_VIDEO_FUNC_01h  01h  ; Set Cursor Shape
-%define  INT_VIDEO_FUNC_02h  02h  ; Set Cursor Position
-%define  INT_VIDEO_FUNC_03h  03h  ; Get Cursor Position And Shape
-%define  INT_VIDEO_FUNC_04h  04h  ; Get Light Pen Position
-%define  INT_VIDEO_FUNC_05h  05h  ; Set Display Page
-%define  INT_VIDEO_FUNC_06h  06h  ; Clear/Scroll Screen Up
-%define  INT_VIDEO_FUNC_07h  07h  ; Clear/Scroll Screen Down
-%define  INT_VIDEO_FUNC_08h  08h  ; Read Character and Attribute at Cursor
-%define  INT_VIDEO_FUNC_09h  09h  ; Write Character and Attribute at Cursor
-%define  INT_VIDEO_FUNC_0Ah  0Ah  ; Write Character at Cursor
-%define  INT_VIDEO_FUNC_0Bh  0Bh  ; Set Border Color
-%define  INT_VIDEO_FUNC_0Ch  0Ch  ; Write Graphics Pixel
-%define  INT_VIDEO_FUNC_0Dh  0Dh  ; Read Graphics Pixel
-%define  INT_VIDEO_FUNC_0Eh  0Eh  ; Write Character in TTY Mode
-%define  INT_VIDEO_FUNC_0Fh  0Fh  ; Get Video Mode
-%define  INT_VIDEO_FUNC_10h  10h  ; Set Palette Registers (EGA, VGA, SVGA)
-%define  INT_VIDEO_FUNC_11h  11h  ; Character Generator (EGA, VGA, SVGA)
-%define  INT_VIDEO_FUNC_12h  12h  ; Alternate Select Functions (EGA, VGA, SVGA)
-%define  INT_VIDEO_FUNC_13h  13h  ; Write String
-%define  INT_VIDEO_FUNC_1Ah  1Ah  ; Get or Set Display Combination Code (VGA, SVGA)
-%define  INT_VIDEO_FUNC_1Bh  1Bh  ; Get Functionality Information (VGA, SVGA)
-%define  INT_VIDEO_FUNC_1Ch  1Ch  ; Save or Restore Video State (VGA, SVGA)
-%define  INT_VIDEO_FUNC_4Fh  4Fh  ; VESA BIOS Extension Functions (SVGA)
+%define  INT_VIDEO_FUNC_00h  0x00  ; Set Video Mode
+%define  INT_VIDEO_FUNC_01h  0x01  ; Set Cursor Shape
+%define  INT_VIDEO_FUNC_02h  0x02  ; Set Cursor Position
+%define  INT_VIDEO_FUNC_03h  0x03  ; Get Cursor Position And Shape
+%define  INT_VIDEO_FUNC_04h  0x04  ; Get Light Pen Position
+%define  INT_VIDEO_FUNC_05h  0x05  ; Set Display Page
+%define  INT_VIDEO_FUNC_06h  0x06  ; Clear/Scroll Screen Up
+%define  INT_VIDEO_FUNC_07h  0x07  ; Clear/Scroll Screen Down
+%define  INT_VIDEO_FUNC_08h  0x08  ; Read Character and Attribute at Cursor
+%define  INT_VIDEO_FUNC_09h  0x09  ; Write Character and Attribute at Cursor
+%define  INT_VIDEO_FUNC_0Ah  0x0A  ; Write Character at Cursor
+%define  INT_VIDEO_FUNC_0Bh  0x0B  ; Set Border Color
+%define  INT_VIDEO_FUNC_0Ch  0x0C  ; Write Graphics Pixel
+%define  INT_VIDEO_FUNC_0Dh  0x0D  ; Read Graphics Pixel
+%define  INT_VIDEO_FUNC_0Eh  0x0E  ; Write Character in TTY Mode
+%define  INT_VIDEO_FUNC_0Fh  0x0F  ; Get Video Mode
+%define  INT_VIDEO_FUNC_10h  0x10  ; Set Palette Registers (EGA, VGA, SVGA)
+%define  INT_VIDEO_FUNC_11h  0x11  ; Character Generator (EGA, VGA, SVGA)
+%define  INT_VIDEO_FUNC_12h  0x12  ; Alternate Select Functions (EGA, VGA, SVGA)
+%define  INT_VIDEO_FUNC_13h  0x13  ; Write String
+%define  INT_VIDEO_FUNC_1Ah  0x1A  ; Get or Set Display Combination Code (VGA, SVGA)
+%define  INT_VIDEO_FUNC_1Bh  0x1B  ; Get Functionality Information (VGA, SVGA)
+%define  INT_VIDEO_FUNC_1Ch  0x1C  ; Save or Restore Video State (VGA, SVGA)
+%define  INT_VIDEO_FUNC_4Fh  0x4F  ; VESA BIOS Extension Functions (SVGA)
 
 ; ========================================
 ; interrupt vector 11h
@@ -47,7 +47,7 @@
 
 ; Returns equipment list
 
-%define  INT_EQP_LIST  11h  ; interrupt vector for interrupt call
+%define  INT_EQP_LIST  0x11  ; interrupt vector for interrupt call
 
 ; ========================================
 ; interrupt vector 12h
@@ -55,100 +55,100 @@
 
 ; Return conventional memory size
 
-%define  INT_CONV_MEM  12h  ; interrupt vector for interrupt call
+%define  INT_CONV_MEM  0x12  ; interrupt vector for interrupt call
 
 ; ========================================
 ; interrupt vector 13h - Low Level Disk Services
 ; ========================================
 
-%define  INT_DISK  13h  ; interrupt vector for interrupt call
+%define  INT_DISK  0x13  ; interrupt vector for interrupt call
 
-%define  INT_DISK_FUNC_00h  00h  ; Reset Disk Drives
-%define  INT_DISK_FUNC_01h  01h  ; Check Drive Status
-%define  INT_DISK_FUNC_02h  02h  ; Read Sectors
-%define  INT_DISK_FUNC_03h  03h  ; Write Sectors
-%define  INT_DISK_FUNC_04h  04h  ; Verify Sectors
-%define  INT_DISK_FUNC_05h  05h  ; Format Track
-%define  INT_DISK_FUNC_08h  08h  ; Get Drive Parameters
-%define  INT_DISK_FUNC_09h  09h  ; Init Fixed Drive Parameters
-%define  INT_DISK_FUNC_0Ch  0Ch  ; Seek To Specified Track
-%define  INT_DISK_FUNC_0Dh  0Dh  ; Reset Fixed Disk Controller
-%define  INT_DISK_FUNC_15h  15h  ; Get Drive Type
-%define  INT_DISK_FUNC_16h  16h  ; Get Floppy Drive Media Change Status
-%define  INT_DISK_FUNC_17h  17h  ; Set Disk Type
-%define  INT_DISK_FUNC_18h  18h  ; Set Floppy Drive Media Type
-%define  INT_DISK_FUNC_41h  41h  ; Extended Disk Drive (EDD) Installation Check
-%define  INT_DISK_FUNC_42h  42h  ; Extended Read Sectors
-%define  INT_DISK_FUNC_43h  43h  ; Extended Write Sectors
-%define  INT_DISK_FUNC_44h  44h  ; Extended Verify Sectors
-%define  INT_DISK_FUNC_45h  45h  ; Lock/Unlock Drive
-%define  INT_DISK_FUNC_46h  46h  ; Eject Media
-%define  INT_DISK_FUNC_47h  47h  ; Extended Seek
-%define  INT_DISK_FUNC_48h  48h  ; Extended Get Drive Parameters
-%define  INT_DISK_FUNC_49h  49h  ; Extended Get Media Change Status
-%define  INT_DISK_FUNC_4Eh  4Eh  ; Extended Set Hardware Configuration
+%define  INT_DISK_FUNC_00h  0x00  ; Reset Disk Drives
+%define  INT_DISK_FUNC_01h  0x01  ; Check Drive Status
+%define  INT_DISK_FUNC_02h  0x02  ; Read Sectors
+%define  INT_DISK_FUNC_03h  0x03  ; Write Sectors
+%define  INT_DISK_FUNC_04h  0x04  ; Verify Sectors
+%define  INT_DISK_FUNC_05h  0x05  ; Format Track
+%define  INT_DISK_FUNC_08h  0x08  ; Get Drive Parameters
+%define  INT_DISK_FUNC_09h  0x09  ; Init Fixed Drive Parameters
+%define  INT_DISK_FUNC_0Ch  0x0C  ; Seek To Specified Track
+%define  INT_DISK_FUNC_0Dh  0x0D  ; Reset Fixed Disk Controller
+%define  INT_DISK_FUNC_15h  0x15  ; Get Drive Type
+%define  INT_DISK_FUNC_16h  0x16  ; Get Floppy Drive Media Change Status
+%define  INT_DISK_FUNC_17h  0x17  ; Set Disk Type
+%define  INT_DISK_FUNC_18h  0x18  ; Set Floppy Drive Media Type
+%define  INT_DISK_FUNC_41h  0x41  ; Extended Disk Drive (EDD) Installation Check
+%define  INT_DISK_FUNC_42h  0x42  ; Extended Read Sectors
+%define  INT_DISK_FUNC_43h  0x43  ; Extended Write Sectors
+%define  INT_DISK_FUNC_44h  0x44  ; Extended Verify Sectors
+%define  INT_DISK_FUNC_45h  0x45  ; Lock/Unlock Drive
+%define  INT_DISK_FUNC_46h  0x46  ; Eject Media
+%define  INT_DISK_FUNC_47h  0x47  ; Extended Seek
+%define  INT_DISK_FUNC_48h  0x48  ; Extended Get Drive Parameters
+%define  INT_DISK_FUNC_49h  0x49  ; Extended Get Media Change Status
+%define  INT_DISK_FUNC_4Eh  0x4E  ; Extended Set Hardware Configuration
 
 ; ========================================
 ; interrupt vector 14h - Serial port services
 ; ========================================
 
-%define  INT_SERIAL  14h  ; interrupt vector for interrupt call
+%define  INT_SERIAL  0x14  ; interrupt vector for interrupt call
 
-%define  INT_SERIAL_FUNC_INIT           00h  ; Serial Port Initialization
-%define  INT_SERIAL_FUNC_TRANSMIT_CHAR  01h  ; Transmit Character
-%define  INT_SERIAL_FUNC_RECEIVE_CHAR   02h  ; Receive Character
-%define  INT_SERIAL_FUNC_STATUS         03h  ; Status
+%define  INT_SERIAL_FUNC_INIT           0x00  ; Serial Port Initialization
+%define  INT_SERIAL_FUNC_TRANSMIT_CHAR  0x01  ; Transmit Character
+%define  INT_SERIAL_FUNC_RECEIVE_CHAR   0x02  ; Receive Character
+%define  INT_SERIAL_FUNC_STATUS         0x03  ; Status
 
 ; ========================================
 ; interrupt vector 15h - Miscellaneous system services
 ; ========================================
 
-%define  INT_MISC  15h  ; interrupt vector for interrupt call
+%define  INT_MISC  0x15  ; interrupt vector for interrupt call
 
-%define  INT_MISC_FUNC_00h  00h  ; Turn on cassette drive motor
-%define  INT_MISC_FUNC_01h  01h  ; Turn off cassette drive motor
-%define  INT_MISC_FUNC_02h  02h  ; Read data blocks from cassette
-%define  INT_MISC_FUNC_03h  03h  ; Write data blocks to cassette
-%define  INT_MISC_FUNC_4Fh  4Fh  ; Keyboard Intercept
-%define  INT_MISC_FUNC_83h  83h  ; Event Wait
-%define  INT_MISC_FUNC_84h  84h  ; Read Joystick
-%define  INT_MISC_FUNC_85h  85h  ; Sysreq Key Callout
-%define  INT_MISC_FUNC_86h  86h  ; Wait
-%define  INT_MISC_FUNC_87h  87h  ; Move Block
-%define  INT_MISC_FUNC_88h  88h  ; Get Extended Memory Size
-%define  INT_MISC_FUNC_89h  89h  ; Switch to Protected Mode
-%define  INT_MISC_FUNC_C0h  0C0h  ; Get System Parameters
-%define  INT_MISC_FUNC_C1h  0C1h  ; Get Extended BIOS Data Area Segment
-%define  INT_MISC_FUNC_C2h  0C2h  ; Pointing Device Functions
-%define  INT_MISC_FUNC_C3h  0C3h  ; Watchdog Timer Functions - PS/2 systems only
-%define  INT_MISC_FUNC_C4h  0C4h  ; Programmable Option Select - MCA bus PS/2 systems only
-%define  INT_MISC_FUNC_D8h  0D8h  ; EISA System Functions - EISA bus systems only
-%define  INT_MISC_FUNC_E8h  0E8h  ; AL=01h , Get Extended Memory Size (Newer function, since 1994). Gives results for memory size above 64 Mb.
-%define  INT_MISC_FUNC_E8h  0E8h  ; AL=20h , Query System Address Map. The information returned from E820 supersedes what is returned from the older AX=E801h and AH=88h interfaces.
+%define  INT_MISC_FUNC_00h  0x00  ; Turn on cassette drive motor
+%define  INT_MISC_FUNC_01h  0x01  ; Turn off cassette drive motor
+%define  INT_MISC_FUNC_02h  0x02  ; Read data blocks from cassette
+%define  INT_MISC_FUNC_03h  0x03  ; Write data blocks to cassette
+%define  INT_MISC_FUNC_4Fh  0x4F  ; Keyboard Intercept
+%define  INT_MISC_FUNC_83h  0x83  ; Event Wait
+%define  INT_MISC_FUNC_84h  0x84  ; Read Joystick
+%define  INT_MISC_FUNC_85h  0x85  ; Sysreq Key Callout
+%define  INT_MISC_FUNC_86h  0x86  ; Wait
+%define  INT_MISC_FUNC_87h  0x87  ; Move Block
+%define  INT_MISC_FUNC_88h  0x88  ; Get Extended Memory Size
+%define  INT_MISC_FUNC_89h  0x89  ; Switch to Protected Mode
+%define  INT_MISC_FUNC_C0h  0xC0  ; Get System Parameters
+%define  INT_MISC_FUNC_C1h  0xC1  ; Get Extended BIOS Data Area Segment
+%define  INT_MISC_FUNC_C2h  0xC2  ; Pointing Device Functions
+%define  INT_MISC_FUNC_C3h  0xC3  ; Watchdog Timer Functions - PS/2 systems only
+%define  INT_MISC_FUNC_C4h  0xC4  ; Programmable Option Select - MCA bus PS/2 systems only
+%define  INT_MISC_FUNC_D8h  0xD8  ; EISA System Functions - EISA bus systems only
+%define  INT_MISC_FUNC_E8h  0xE8  ; AL=01h , Get Extended Memory Size (Newer function, since 1994). Gives results for memory size above 64 Mb.
+%define  INT_MISC_FUNC_E8h  0xE8  ; AL=20h , Query System Address Map. The information returned from E820 supersedes what is returned from the older AX=E801h and AH=88h interfaces.
 
 ; ========================================
 ; interrupt vector 16h - Keyboard services
 ; ========================================
 
-%define  INT_KEYBOARD  16h  ; interrupt vector for interrupt call
+%define  INT_KEYBOARD  0x16  ; interrupt vector for interrupt call
 
-%define  INT_KEYBOARD_FUNC_00h  00h  ; Read Character
-%define  INT_KEYBOARD_FUNC_01h  01h  ; Read Input Status
-%define  INT_KEYBOARD_FUNC_02h  02h  ; Read Keyboard Shift Status
-%define  INT_KEYBOARD_FUNC_05h  05h  ; Store Keystroke in Keyboard Buffer
-%define  INT_KEYBOARD_FUNC_10h  10h  ; Read Character Extended
-%define  INT_KEYBOARD_FUNC_11h  11h  ; Read Input Status Extended
-%define  INT_KEYBOARD_FUNC_12h  12h  ; Read Keyboard Shift Status Extended
+%define  INT_KEYBOARD_FUNC_00h  0x00  ; Read Character
+%define  INT_KEYBOARD_FUNC_01h  0x01  ; Read Input Status
+%define  INT_KEYBOARD_FUNC_02h  0x02  ; Read Keyboard Shift Status
+%define  INT_KEYBOARD_FUNC_05h  0x05  ; Store Keystroke in Keyboard Buffer
+%define  INT_KEYBOARD_FUNC_10h  0x10  ; Read Character Extended
+%define  INT_KEYBOARD_FUNC_11h  0x11  ; Read Input Status Extended
+%define  INT_KEYBOARD_FUNC_12h  0x12  ; Read Keyboard Shift Status Extended
 
 ; ========================================
 ; interrupt vector 17h - Printer services
 ; ========================================
 
-%define  INT_PRINTER  17h  ; interrupt vector for interrupt call
+%define  INT_PRINTER  0x17  ; interrupt vector for interrupt call
 
-%define  INT_PRINTER_FUNC_PRINT_CHAR    00h  ; Print Character to Printer
-%define  INT_PRINTER_FUNC_INIT          01h  ; Initialize Printer
-%define  INT_PRINTER_FUNC_CHECK_STATUS  02h  ; Check Printer Status
+%define  INT_PRINTER_FUNC_PRINT_CHAR    0x00  ; Print Character to Printer
+%define  INT_PRINTER_FUNC_INIT          0x01  ; Initialize Printer
+%define  INT_PRINTER_FUNC_CHECK_STATUS  0x02  ; Check Printer Status
 
 ; ========================================
 ; interrupt vector 18h
@@ -166,35 +166,35 @@
 ; interrupt vector 1Ah - Real Time Clock Services
 ; ========================================
 
-%define  INT_RTC  1Ah  ; interrupt vector for interrupt call
+%define  INT_RTC  0x1A  ; interrupt vector for interrupt call
 
-%define  INT_RTC_FUNC_READ         00h  ; Read RTC
-%define  INT_RTC_FUNC_SET          01h  ; Set RTC
-%define  INT_RTC_FUNC_READ_TIME    02h  ; Read RTC Time
-%define  INT_RTC_FUNC_SET_TIME     03h  ; Set RTC Time
-%define  INT_RTC_FUNC_READ_DATE    04h  ; Read RTC Date
-%define  INT_RTC_FUNC_SET_DATE     05h  ; Set RTC Date
-%define  INT_RTC_FUNC_SET_ALARM    06h  ; Set RTC Alarm
-%define  INT_RTC_FUNC_RESET_ALARM  07h  ; Reset RTC Alarm
+%define  INT_RTC_FUNC_READ         0x00  ; Read RTC
+%define  INT_RTC_FUNC_SET          0x01  ; Set RTC
+%define  INT_RTC_FUNC_READ_TIME    0x02  ; Read RTC Time
+%define  INT_RTC_FUNC_SET_TIME     0x03  ; Set RTC Time
+%define  INT_RTC_FUNC_READ_DATE    0x04  ; Read RTC Date
+%define  INT_RTC_FUNC_SET_DATE     0x05  ; Set RTC Date
+%define  INT_RTC_FUNC_SET_ALARM    0x06  ; Set RTC Alarm
+%define  INT_RTC_FUNC_RESET_ALARM  0x07  ; Reset RTC Alarm
 
 ; ========================================
 ; interrupt vector 1Ah - PCI Services - implemented by BIOSes supporting PCI 2.0 or later
 ; ========================================
 
-%define  INT_PCI  1Ah  ; interrupt vector for interrupt call
+%define  INT_PCI  0x1A  ; interrupt vector for interrupt call
 
-%define  INT_PCI_FUNC_B101h  0B101h  ; PCI Installation Check
-%define  INT_PCI_FUNC_B102h  0B102h  ; Find PCI Device
-%define  INT_PCI_FUNC_B103h  0B103h  ; Find PCI Class Code
-%define  INT_PCI_FUNC_B106h  0B106h  ; PCI Bus-Specific Operations
-%define  INT_PCI_FUNC_B108h  0B108h  ; Read Configuration Byte
-%define  INT_PCI_FUNC_B109h  0B109h  ; Read Configuration Word
-%define  INT_PCI_FUNC_B10Ah  0B10Ah  ; Read Configuration Dword
-%define  INT_PCI_FUNC_B10Bh  0B10Bh  ; Write Configuration Byte
-%define  INT_PCI_FUNC_B10Ch  0B10Ch  ; Write Configuration Word
-%define  INT_PCI_FUNC_B10Dh  0B10Dh  ; Write Configuration Dword
-%define  INT_PCI_FUNC_B10Eh  0B10Eh  ; Get IRQ Routine Information
-%define  INT_PCI_FUNC_B10Fh  0B10Fh  ; Set PCI IRQ
+%define  INT_PCI_FUNC_B101h  0xB101  ; PCI Installation Check
+%define  INT_PCI_FUNC_B102h  0xB102  ; Find PCI Device
+%define  INT_PCI_FUNC_B103h  0xB103  ; Find PCI Class Code
+%define  INT_PCI_FUNC_B106h  0xB106  ; PCI Bus-Specific Operations
+%define  INT_PCI_FUNC_B108h  0xB108  ; Read Configuration Byte
+%define  INT_PCI_FUNC_B109h  0xB109  ; Read Configuration Word
+%define  INT_PCI_FUNC_B10Ah  0xB10A  ; Read Configuration Dword
+%define  INT_PCI_FUNC_B10Bh  0xB10B  ; Write Configuration Byte
+%define  INT_PCI_FUNC_B10Ch  0xB10C  ; Write Configuration Word
+%define  INT_PCI_FUNC_B10Dh  0xB10D  ; Write Configuration Dword
+%define  INT_PCI_FUNC_B10Eh  0xB10E  ; Get IRQ Routine Information
+%define  INT_PCI_FUNC_B10Fh  0xB10F  ; Set PCI IRQ
 
 ; ========================================
 ; interrupt vector 1Bh
