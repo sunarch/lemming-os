ba0bb7f74a77e10930cc701b3c9776630f868d06
András Németh <sunarch@protonmail.com>
2020-10-15 19:08:08 +0200

    Change license to Mozilla Public License 2.0

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ It gets its name after the animal with the same name.
 See: https://en.wikipedia.org/wiki/Lemming
 
 This is not a real, full-feature OS, just a project for tinkering.
+
+### License
+[Mozilla Public License 2.0](https://spdx.org/licenses/MPL-2.0.html)
--- bootloader/data.asm
+++ bootloader/data.asm
@@ -1,3 +1,7 @@
+; This Source Code Form is subject to the terms of the Mozilla Public
+; License, v. 2.0. If a copy of the MPL was not distributed with this
+; file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 
 str_welcome      db  'Welcome to LemmingOS!', ASCII_CR, ASCII_LF, ASCII_NUL
 str_bad_command  db  'Bad command entered.', ASCII_CR, ASCII_LF, ASCII_NUL
