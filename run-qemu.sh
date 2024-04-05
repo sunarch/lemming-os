87e7cddd1be76856e7753f2d8c3e57bd160aed19
András Németh <sunarch@protonmail.com>
2024-04-06 00:25:05 +0200

    Add Qemu script

---
+++ run-qemu.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+ISO='./build/LemmingOS.bin'
+RAM='1G'
+
+qemu-system-x86_64 \
+    -drive file="${ISO}",media=disk,format=raw \
+    -m "${RAM}"
