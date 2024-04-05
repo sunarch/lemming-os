68fee2258f0e6979567054a5f93ecb5b919a838f
András Németh <sunarch@protonmail.com>
2024-04-06 01:38:13 +0200

    Adjust command prompt

--- bootloader/data.asm
+++ bootloader/data.asm
@@ -6,7 +6,7 @@
 
 str_welcome      db  'Welcome to LemmingOS!', ASCII_CR, ASCII_LF, ASCII_NUL
 str_bad_command  db  'Bad command entered.', ASCII_CR, ASCII_LF, ASCII_NUL
-str_prompt       db  '>', ASCII_NUL
+str_prompt       db  '> ', ASCII_NUL
 str_version      db  "LemmingOS v.0.1.1 Alpha", ASCII_CR, ASCII_LF, ASCII_NUL
 
 ; ============================================================================ ;
