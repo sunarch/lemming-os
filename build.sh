17da6e9dc65f37e49fb575443480c5872962ed05
András Németh <sunarch@protonmail.com>
2024-04-06 06:35:19 +0200

    Restructure build script

--- build.sh
+++ build.sh
@@ -4,27 +4,44 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+BUILD_DIR='./build/'
+
+BOOTLOADER_BIN="${BUILD_DIR}bootloader.bin"
+KERNEL_BIN="${BUILD_DIR}kernel.bin"
+HDD_FILE="${BUILD_DIR}LemmingOS.hdd"
+
 # remove if output exists
-rm 'build/bootloader.bin'
-rm 'build/kernel.bin'
-rm 'build/LemmingOS.hdd'
+rm "${BOOTLOADER_BIN}"
+rm "${KERNEL_BIN}"
+rm "${HDD_FILE}"
 
 # ---------------------------------------------------------------------------- #
 # bootloader
 
+BOOTLOADER_DIR='./bootloader/'
+BOOTLOADER_SRC="${BOOTLOADER_DIR}bootloader.asm"
+BOOTLOADER_SIZE=512
+
 # assemble
-nasm './bootloader/bootloader.asm' -i './macros/' -i './bootloader/' -f bin -o './build/bootloader.bin'
- 
+nasm "${BOOTLOADER_SRC}" -f bin -o "${BOOTLOADER_BIN}" \
+     -i "${BOOTLOADER_DIR}" \
+     -i './macros/'
+
 # copy to bootable hdd image
-dd if='build/bootloader.bin' of='build/LemmingOS.hdd' bs=512 count=1
+dd if="${BOOTLOADER_BIN}" of="${HDD_FILE}" bs=$BOOTLOADER_SIZE count=1
  
 # ---------------------------------------------------------------------------- #
 # kernel
 
+KERNEL_DIR='./kernel/'
+KERNEL_SRC="${KERNEL_DIR}kernel.asm"
+KERNEL_SIZE=512
+
 # assemble
-nasm './kernel/kernel.asm' -i './kernel/' -f bin -o './build/kernel.bin'
+nasm "${KERNEL_SRC}" -f bin -o "${KERNEL_BIN}" \
+     -i "${KERNEL_DIR}"
 
 # copy to bootable hdd image
-dd if='build/kernel.bin' of='build/LemmingOS.hdd' obs=512 oseek=1
+dd if="${KERNEL_BIN}" of="${HDD_FILE}" obs=$KERNEL_SIZE oseek=1
 
 # ---------------------------------------------------------------------------- #
